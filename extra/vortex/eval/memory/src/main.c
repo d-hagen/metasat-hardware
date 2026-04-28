13474f360d8fdbbb9fb8a4afc054ba8a2630a60a
dan <danhagen.2001@gmail.com>
2026-04-28 14:01:39 +0000

    memory test code update

--- extra/vortex/eval/memory/src/main.c
+++ extra/vortex/eval/memory/src/main.c
@@ -18,7 +18,8 @@ int main()
     uint8_t *output = (uint8_t*) malloc(num_points * sizeof(uint8_t));
     for (int i = 0; i < num_points; ++i) input[i] = 0;
     for (int i = 0; i < num_points; ++i) output[i] = 0;
-    vx_device_h device;
+    vx_device_h device= NULL;
+    vx_buffer_h src_buffer = NULL;
     puts("Start connection with the GPU");
     vx_dev_open(&device);
     puts("Retrieving GPU configuration:");
@@ -28,21 +29,22 @@ int main()
     vx_dev_caps(device, VX_CAPS_NUM_CORES, &ncores);
     printf("    - %ld cores\n    - %ld warps\n    - %ld threads\n", ncores, nwarps, nthreads);
     printf("Allocate device memory (%d bytes)\n", num_points * sizeof(uint8_t));
-    uint64_t src;
-    vx_mem_alloc(device, num_points * sizeof(uint8_t), VX_MEM_TYPE_GLOBAL, &src);
+    uint64_t src_addr;
+    vx_mem_alloc(device, num_points * sizeof(uint8_t), VX_MEM_READ, &src_buffer);
+    vx_mem_address(src_buffer, &src_addr);
     puts("Clear device memory");
-    vx_copy_to_dev(device, src, input, num_points * sizeof(uint8_t));
+    vx_copy_to_dev(src_buffer, input,0, num_points * sizeof(uint8_t));
     puts("Verify memory cleaned");
     uint8_t is_vx_cleaned = 0;
-    vx_copy_from_dev(device, output, src, num_points * sizeof(uint8_t));
+    vx_copy_from_dev(output, src_buffer, 0, num_points * sizeof(uint8_t));
     for (int i = 0; i < num_points; ++i) if(output[i] != 0) goto error_print;
     for (int i = 0; i < num_points; ++i) input[i] = i;
     printf("Send data to the device (%d bytes)\n", num_points * sizeof(uint8_t));
-    vx_copy_to_dev(device, src, input, num_points * sizeof(uint8_t));
+    vx_copy_to_dev(src_buffer, input, 0, num_points * sizeof(uint8_t));
     printf("Retrieving data from the device (%d bytes)\n", num_points * sizeof(uint8_t));
-    vx_copy_from_dev(device, output, src, num_points * sizeof(uint8_t));
+    vx_copy_from_dev(output, src_buffer, 0, num_points * sizeof(uint8_t));
     printf("Free allocated memory (%d bytes)\n", 1 * num_points * sizeof(uint8_t));
-    vx_mem_free(device, src);
+    vx_mem_free(src_buffer);
     puts("Close connection with the GPU");
     vx_dev_close(device);
     is_vx_cleaned = 1;
@@ -59,7 +61,7 @@ error_print:
 cleanup:
 
     if (!is_vx_cleaned) {
-        vx_mem_free(device, src);
+        vx_mem_free(src_buffer);
         vx_dev_close(device);
     }
 
