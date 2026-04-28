f837cb6d98f8ad4b15c272de4571b3fa40acb0ed
dan <danhagen.2001@gmail.com>
2026-04-28 13:44:01 +0000

    evaluation test code update

--- extra/vortex/eval/evaluation/src/kernel.c
+++ extra/vortex/eval/evaluation/src/kernel.c
@@ -2,18 +2,21 @@
 #include <vx_intrinsics.h>
 #include <vx_spawn.h>
 
-#define KERNEL_ARG_DEV_MEM_ADDR 0x5ffff000
+#include <VX_types.h>
 
-void kernel(uint8_t task_id, uint64_t* __UNIFORM__ arg)
+void kernel(void *arg)
 {
-    uint8_t *src = (uint8_t *) arg[1];
-    uint8_t *dest = (uint8_t *) arg[2];
+    uint64_t *karg = (uint64_t *)arg;
+    uint8_t *src = (uint8_t *)karg[1];
+    uint8_t *dest = (uint8_t *)karg[2];
 
-    dest[task_id] = (uint8_t)(src[task_id] * 2);
+    uint32_t tid = blockIdx.x;
+    dest[tid] = (uint8_t)(src[tid] * 2);
 }
 
 int main()
 {
-    uint64_t* arg = (uint64_t*) (KERNEL_ARG_DEV_MEM_ADDR);
-    vx_spawn_tasks(arg[0], (vx_spawn_tasks_cb)kernel, arg);
+    uint64_t *arg = (uint64_t *)csr_read(VX_CSR_MSCRATCH);
+    uint32_t num_tasks = (uint32_t)arg[0];
+    return vx_spawn_threads(1, &num_tasks, 0, (vx_kernel_func_cb)kernel, arg);
 }
--- extra/vortex/eval/evaluation/src/main.c
+++ extra/vortex/eval/evaluation/src/main.c
@@ -3,7 +3,6 @@
 #include <stdint.h>
 #include <vortex.h>
 
-#define KERNEL_ARG_DEV_MEM_ADDR 0x5ffff000
 
 #ifndef VERBOSE
 #define VERBOSE 0
@@ -23,7 +22,11 @@ int main()
     uint8_t *output = (uint8_t*) malloc(num_points * sizeof(uint8_t));
     for (int i = 0; i < num_points; ++i) input[i] = i;
     for (int i = 0; i < num_points; ++i) output[i] = 0;
-    vx_device_h device;
+    vx_device_h device = NULL;
+    vx_buffer_h src_buffer = NULL;
+    vx_buffer_h dst_buffer = NULL;
+    vx_buffer_h krnl_buffer = NULL;
+    vx_buffer_h args_buffer = NULL;
     puts("Start connection with the GPU");
     vx_dev_open(&device);
     puts("Retrieving GPU configuration:");
@@ -34,27 +37,31 @@ int main()
     printf("    - %ld cores\n    - %ld warps\n    - %ld threads\n", ncores, nwarps, nthreads);
     size_t vx_kernel_size = vx_kernel_end - vx_kernel_start;
     printf("Upload kernel to the device (%d bytes)\n", vx_kernel_size);
-    vx_upload_kernel_bytes(device, vx_kernel_start, vx_kernel_size);
+    vx_upload_kernel_bytes(device, vx_kernel_start, vx_kernel_size, &krnl_buffer);
     printf("Allocate device memory (%d bytes)\n", num_points * sizeof(uint8_t) * 2);
-    uint64_t src, dest;
-    vx_mem_alloc(device, num_points * sizeof(uint8_t), VX_MEM_TYPE_GLOBAL, &src);
-    vx_mem_alloc(device, num_points * sizeof(uint8_t), VX_MEM_TYPE_GLOBAL, &dest);
+    uint64_t src_addr, dest_addr;
+    vx_mem_alloc(device, num_points * sizeof(uint8_t), VX_MEM_READ, &src_buffer);
+    vx_mem_address(src_buffer, &src_addr);
+    vx_mem_alloc(device, num_points * sizeof(uint8_t), VX_MEM_WRITE, &dst_buffer);
+    vx_mem_address(dst_buffer, &dest_addr);
     puts("Upload kernel arguments");
-    uint64_t arg[3] = {num_points, src, dest};
-    vx_copy_to_dev(device, KERNEL_ARG_DEV_MEM_ADDR, arg, 3*sizeof(uint64_t));
+    uint64_t arg[3] = {num_points, src_addr, dest_addr};
+    vx_upload_bytes(device, arg, sizeof(arg), &args_buffer);
     printf("Send input data to the device (%d bytes)\n", num_points * sizeof(uint8_t));
-    vx_copy_to_dev(device, src, input, num_points * sizeof(uint8_t));
+    vx_copy_to_dev(src_buffer, input, 0, num_points * sizeof(uint8_t));
     puts("Clear destination memory");
-    vx_copy_to_dev(device, dest, output, num_points * sizeof(uint8_t));
+    vx_copy_to_dev(dst_buffer, output, 0, num_points * sizeof(uint8_t));
     puts("Start execution");
-    vx_start(device);
+    vx_start(device, krnl_buffer, args_buffer);
     puts("Waiting for execution to complete");
     vx_ready_wait(device, VX_MAX_TIMEOUT);
     printf("Retrieving data from the device (%d bytes)\n", num_points * sizeof(uint8_t));
-    vx_copy_from_dev(device, output, dest, num_points * sizeof(uint8_t));
+    vx_copy_from_dev(output, dst_buffer, 0, num_points * sizeof(uint8_t));
     printf("Free allocated memory (%d bytes)\n", 2 * num_points * sizeof(uint8_t));
-    vx_mem_free(device, src);
-    vx_mem_free(device, dest);
+    vx_mem_free(src_buffer);
+    vx_mem_free(dst_buffer);
+    vx_mem_free(krnl_buffer);
+    vx_mem_free(args_buffer);
     puts("Close connection with the GPU");
     vx_dev_close(device);
     for (int i = 0; i < num_points; ++i) {
