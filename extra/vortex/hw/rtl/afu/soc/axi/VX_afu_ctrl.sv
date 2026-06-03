5e6a0a9dbc153a1b2829bb6ac1210bac6c17e110
Dan Joshua Hagen <danhagen.2001@gmail.com>
2026-06-03 12:40:24 +0200

    Update BSC AFU adapter for Vortex 2.2 LMEM rename

--- extra/vortex/hw/rtl/afu/soc/axi/VX_afu_ctrl.sv
+++ extra/vortex/hw/rtl/afu/soc/axi/VX_afu_ctrl.sv
@@ -136,7 +136,7 @@ module VX_afu_ctrl #(
 	reg  [31:0] cmd_size;
 	wire [31:0] cmd_rdata;
 	wire [63:0] dev_caps = {16'b0,
-		8'(`SM_ENABLED ? `SMEM_LOG_SIZE : 0),
+		8'(`LMEM_ENABLED ? `LMEM_LOG_SIZE : 0),
 		16'(`NUM_CORES * `NUM_CLUSTERS), 
 		8'(`NUM_WARPS), 
 		8'(`NUM_THREADS), 
