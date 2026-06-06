a37be017ae5fb7b7855410e1fb30cc6d27240009
Dan Joshua Hagen <danhagen.2001@gmail.com>
2026-06-06 18:55:04 +0200

    nospawn: remove vx_tmc_zero bypass - test natural return through _Exit

--- extra/vortex/eval/evaluation/src/kernel_nospawn.c
+++ extra/vortex/eval/evaluation/src/kernel_nospawn.c
@@ -24,7 +24,5 @@ int main()
     uint8_t  *src  = (uint8_t *)arg[1];
     uint8_t  *dest = (uint8_t *)arg[2];
     dest[0] = (uint8_t)(src[0] * 2);
-    __asm__ volatile ("fence");  // drain pending stores before deactivating
-    vx_tmc_zero();
-    __builtin_unreachable();
+    return 0;
 }
