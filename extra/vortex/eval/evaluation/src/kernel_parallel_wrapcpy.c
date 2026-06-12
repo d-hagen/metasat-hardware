d485cfd10d403b0bfe6b91365c08d0108139ff06
dan <danhagen.2001@gmail.com>
2026-06-12 13:25:00 +0000

    Addition of wrapset/wrapcpy isolation variants: byte-loop for only one of memset/memcpy to localize the nowrap stall

---
+++ extra/vortex/eval/evaluation/src/kernel_parallel_wrapcpy.c
@@ -0,0 +1,54 @@
+// parallel (byte-loop memcpy only; newlib memset)
+#include <stdint.h>
+#include <vx_intrinsics.h>
+#include <VX_types.h>
+
+extern "C" __attribute__((used)) void *__wrap_memcpy(void *d, const void *s, __SIZE_TYPE__ n) {
+    char *dp = (char *)d; const char *sp = (const char *)s;
+    while (n--) *dp++ = *sp++; return d;
+}
+
+static void __attribute__((noinline)) worker_stub(void) {
+    vx_tmc(-1);
+    uint64_t arg_addr;
+    __asm__ volatile ("csrr %0, mscratch" : "=r"(arg_addr));
+    uint64_t *arg  = (uint64_t *)arg_addr;
+    uint32_t n     = (uint32_t)arg[0];
+    uint8_t  *src  = (uint8_t *)arg[1];
+    uint8_t  *dest = (uint8_t *)arg[2];
+    uint32_t gtid;
+    __asm__ volatile ("csrr %0, mhartid" : "=r"(gtid));
+    uint32_t stride = vx_num_warps() * vx_num_threads();
+    for (uint32_t i = gtid; i < n; i += stride) {
+        dest[i] = (uint8_t)(src[i] * 2);
+    }
+    vx_tmc_zero();
+}
+
+int main() {
+    vx_wspawn(4, worker_stub);
+
+    vx_tmc(-1);
+    uint64_t arg_addr;
+    __asm__ volatile ("csrr %0, mscratch" : "=r"(arg_addr));
+    uint64_t *arg  = (uint64_t *)arg_addr;
+    uint32_t n     = (uint32_t)arg[0];
+    uint8_t  *src  = (uint8_t *)arg[1];
+    uint8_t  *dest = (uint8_t *)arg[2];
+    uint32_t gtid;
+    __asm__ volatile ("csrr %0, mhartid" : "=r"(gtid));
+    uint32_t stride = vx_num_warps() * vx_num_threads();
+    for (uint32_t i = gtid; i < n; i += stride) {
+        dest[i] = (uint8_t)(src[i] * 2);
+    }
+
+    vx_tmc(1);
+
+    uint32_t aw;
+    do {
+        __asm__ volatile ("csrr %0, %1" : "=r"(aw) : "i"(VX_CSR_ACTIVE_WARPS));
+    } while (aw != 1);
+
+    vx_tmc_zero();
+    __builtin_unreachable();
+}
