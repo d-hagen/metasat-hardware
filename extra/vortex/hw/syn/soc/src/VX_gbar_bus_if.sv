ca9440b31e4753b9ebda4b53cc8a971653ea39c5
dan <danhagen.2001@gmail.com>
2026-06-22 09:53:06 +0000

    Regenerate Vortex RTL for NUM_CORES=8 (2 sockets x 4 cores) for the 8-core wrong-data sim hunt

--- extra/vortex/hw/syn/soc/src/VX_gbar_bus_if.sv
+++ extra/vortex/hw/syn/soc/src/VX_gbar_bus_if.sv
@@ -1,8 +1,8 @@
 interface VX_gbar_bus_if ();
     wire                    req_valid;
     wire [((($clog2(4)) != 0) ? ($clog2(4)) : 1)-1:0]    req_id;
-    wire [((($clog2(2)) != 0) ? ($clog2(2)) : 1)-1:0]    req_size_m1;
-    wire [((($clog2(2)) != 0) ? ($clog2(2)) : 1)-1:0]    req_core_id;
+    wire [((($clog2(8)) != 0) ? ($clog2(8)) : 1)-1:0]    req_size_m1;
+    wire [((($clog2(8)) != 0) ? ($clog2(8)) : 1)-1:0]    req_core_id;
     wire                    req_ready;
     wire                    rsp_valid;
     wire [((($clog2(4)) != 0) ? ($clog2(4)) : 1)-1:0]    rsp_id;
